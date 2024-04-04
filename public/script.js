2f9e8ada6349023f4bad0c19b7ca939eca4a1f7c
biswasprasana001 <biswasprasana004@gmail.com>
2024-04-04 17:55:29 +0530

    Added Comments

--- public/script.js
+++ public/script.js
@@ -1,53 +1,100 @@
+// public\script.js
+
+// Establish a connection to the server
 const socket = io('/')
+
+// Get the HTML element with the ID 'video-grid'
 const videoGrid = document.getElementById('video-grid')
+
+// Create a new Peer object
 const myPeer = new Peer()
+
+// Create a new video element
 const myVideo = document.createElement('video')
+
+// Mute our own video stream so we don't hear ourselves
 myVideo.muted = true
+
+// Create an object to keep track of peers
 const peers = {}
+
+// Request access to the user's video and audio
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
+  // Add our video stream to the video element and play it
   addVideoStream(myVideo, stream)
 
+  // When we receive a call
   myPeer.on('call', call => {
+    // Answer the call with our stream
     call.answer(stream)
+
+    // Create a new video element
     const video = document.createElement('video')
+
+    // When we receive their stream
     call.on('stream', userVideoStream => {
+      // Add their video stream to the video element and play it
       addVideoStream(video, userVideoStream)
     })
   })
 
+  // When a new user connects
   socket.on('user-connected', userId => {
+    // Connect to the new user
     connectToNewUser(userId, stream)
   })
 })
 
+// When a user disconnects
 socket.on('user-disconnected', userId => {
+  // If we have information on that peer, close the connection
   if (peers[userId]) peers[userId].close()
 })
 
+// When our peer connection is open
 myPeer.on('open', id => {
+  // Join the room and send our ID
   socket.emit('join-room', ROOM_ID, id)
 })
 
+// Function to connect to a new user
 function connectToNewUser(userId, stream) {
+  // Call the new user with our stream
   const call = myPeer.call(userId, stream)
+
+  // Create a new video element
   const video = document.createElement('video')
+
+  // When we receive their stream
   call.on('stream', userVideoStream => {
+    // Add their video stream to the video element and play it
     addVideoStream(video, userVideoStream)
   })
+
+  // When the call ends
   call.on('close', () => {
+    // Remove their video element
     video.remove()
   })
 
+  // Add the call to our peers object
   peers[userId] = call
 }
 
+// Function to add a video stream
 function addVideoStream(video, stream) {
+  // Set the source of the video element to our stream
   video.srcObject = stream
+
+  // When the video data is loaded
   video.addEventListener('loadedmetadata', () => {
+    // Play the video
     video.play()
   })
+
+  // Add the video element to the video grid
   videoGrid.append(video)
 }
--- server.js
+++ server.js
@@ -1,29 +1,55 @@
+// server.js
+
+// Import the express module
 const express = require('express')
+
+// Create a new express application
 const app = express()
+
+// Create a new HTTP server and pass the express app to it
 const server = require('http').Server(app)
+
+// Create a new Socket.IO instance and pass the HTTP server to it
 const io = require('socket.io')(server)
+
+// Import the UUID module
 const { v4: uuidV4 } = require('uuid')
 
+// Set the view engine to EJS
 app.set('view engine', 'ejs')
+
+// Serve static files from the 'public' directory
 app.use(express.static('public'))
 
+// Define a route handler for the home page
 app.get('/', (req, res) => {
+  // Redirect to a new room
   res.redirect(`/${uuidV4()}`)
 })
 
+// Define a route handler for rooms
 app.get('/:room', (req, res) => {
+  // Render the room view and pass the room ID to it
   res.render('room', { roomId: req.params.room })
 })
 
+// When a client connects
 io.on('connection', socket => {
+  // When a client wants to join a room
   socket.on('join-room', (roomId, userId) => {
+    // Join the room
     socket.join(roomId)
+
+    // Notify the other clients in the room that a new user has connected
     socket.broadcast.to(roomId).emit('user-connected', userId)
 
+    // When the client disconnects
     socket.on('disconnect', () => {
+      // Notify the other clients in the room that the user has disconnected
       socket.broadcast.to(roomId).emit('user-disconnected', userId)
     })
   })
 })
 
+// Start the server on port 3000
 server.listen(3000)
